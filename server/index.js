e67873480c89c286e64681f963031a5a77df5865
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-11-24 15:40:17 +0530

    changes
    ch

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
